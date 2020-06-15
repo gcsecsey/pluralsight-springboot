58b04fc4186ef583ba19300ea12f508716b007ed
gcsecsey <gcsecsey@gmail.com>
2020-06-15 21:13:38 +0200

    Initial commit

---
+++ src/main/java/com/pluralsight/springbootconference/SpringbootConferenceApplication.java
@@ -0,0 +1,13 @@
+package com.pluralsight.springbootconference;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootConferenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootConferenceApplication.class, args);
+	}
+
+}
